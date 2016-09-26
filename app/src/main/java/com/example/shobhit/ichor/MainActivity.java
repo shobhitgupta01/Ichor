b07216547b585206642185c71af967592311b963
shobhitgupta01 <shobhit.gupta1995@gmail.com>
2016-09-26 17:46:02 +0530

    Added firebase authentication

--- app/src/main/java/com/example/shobhit/ichor/MainActivity.java
+++ app/src/main/java/com/example/shobhit/ichor/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.shobhit.ichor;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btnLogout;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Button
+        btnLogout=(Button)findViewById(R.id.button6);
+
+        //Progress Bar
+        progressBar=(ProgressBar)findViewById(R.id.progressBar4);
+
+        //Firebase Instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+            }
+        }
+    };
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+    }//onCreate ends
+        //sign out method
+        public void signOut() {
+            auth.signOut();
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            auth.addAuthStateListener(authStateListener);
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            if (authStateListener != null) {
+                auth.removeAuthStateListener(authStateListener);
+            }
+        }
 }
